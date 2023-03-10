3e26f1f09a0647cdf16eea43caa1d987a2fc85fb
Kristen von Gerichten <kristenvongerichten@Kristens-MBP.lan>
2023-03-10 18:41:16 -0500

    created routes, seeded data, stopped at testing with Postman

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-require ('dotenv').config
+// DEPENDENCIES
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+// CONFIGURATION
+require('dotenv').config();
+console.log(process.env.PORT);
+console.log(process.env.MONGO_URI);
+const PORT = process.env.PORT
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('connected to mongo: ', process.env.MONGO_URI);
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('This is my books-api!')
 })
 
+// BOOKS
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
+
+// 404 PAGE
 app.get('*', (req, res) => {
     res.send('<h1>404 Page<h1>')
 })
 
+// LISTEN
 app.listen(process.env.PORT, () => {
-    console.log("Server listening on port 3000")
+    console.log(`Server listening on port ${PORT}`)
 })
